d02f958505e4edf2088e6b41620999e11e2127cf
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-05-16 08:56:22 -0300

    finished

---
+++ Assets/RestartSceneOnFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneOnFall : MonoBehaviour
+{
+    public float deathFall = -15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y  < deathFall)
+        {
+            Debug.Log("respawn");
+            SceneManager.LoadScene("Fps Parkour");
+        }
+    }
+}
